268594855d44555efc78e08148796758f44c4358
kongxueyang <992183887@qq.com>
2019-03-19 00:40:32 +0800

    vue.config.js最终版

--- vue.config.js
+++ vue.config.js
@@ -2,32 +2,22 @@
 const glob = require('glob')
 const path = require('path')
 const fs = require('fs')
-const PAGES_PATH = path.resolve(__dirname, './src/views')
+const PAGES_PATH = path.join(__dirname, './src/views')
 const webpack = require("webpack")
 const pages = {}
-glob.sync(PAGES_PATH + '/*/!(*(router)).js').forEach(filepath => {
-    
-    const pageName = path.basename(path.dirname(filepath))
-    const templateName = path.basename(filepath)
-    
-    const entry = `${pageName}_${templateName}`.replace('.js', '')
-    let template = path.dirname(filepath) + '/' + templateName.replace('.js', '.html')
-
+glob.sync(PAGES_PATH +'/**/!(router).js' ).forEach(filepath => {    
+    let template = filepath.replace('.js', '.html')
     if (!fs.existsSync(template)) {
         template = 'public/index.html'
     }
-
-    pages[entry] = {
+    const pageEntry = path.relative(PAGES_PATH, filepath).replace('.js', '')
+    pages[pageEntry] = {
         entry: filepath,
-        filename: `${pageName}/${templateName.replace('.js', '.html')}`,
+        filename: `${pageEntry}.html`,
         template: template
     }
-     
 })
 
-// pages['index'] = {
-//     entry: 'src/views/index.js'
-// }
 
 module.exports = {
     pages,
